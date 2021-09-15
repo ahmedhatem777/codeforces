d15f59f0b2084cc55a28dd0b01aa49f558615b8b
Ahmed Hatem <ahmedhatem777@hotmail.com>
2021-09-15 03:25:08 +0200

    Soldier and Bananas

---
+++ soldier_bananas.py
@@ -0,0 +1,17 @@
+k_n_w = input().split();
+initial_cost = int(k_n_w[0]);
+money = int(k_n_w[1]);
+bananas = int(k_n_w[2]);
+total_cost = 0;
+i = 1;
+
+while i < bananas + 1:
+    total_cost = total_cost + (initial_cost * i);
+    i += 1;
+
+money_to_borrow = total_cost - money;
+
+if money_to_borrow > 0:
+    print(money_to_borrow)
+else:
+    print(0);
