0646e6d4cbd89406918492584cf2cf274d0f5d3b
Ahmed Hatem <ahmedhatem777@hotmail.com>
2021-10-06 23:54:39 +0200

    Boring Apartments

--- boring_apartments.py
+++ boring_apartments.py
@@ -1,17 +1,9 @@
-# https://codeforces.com/problemset/problem/1520/A
+# https://codeforces.com/problemset/problem/1433/A
 test_cases = int(input())
 
 for i in range(test_cases):
-    days = int(input())
-    tasks = list(input())
-    unique_tasks = set()
-    flag = True
-
-    for i in range(days):
-        if tasks[i] in unique_tasks and tasks[i] != tasks[i - 1]:
-            flag = not flag
-            break
-        else:
-            unique_tasks.add(tasks[i])
-
-    print('YES' if flag else 'NO')
+    apartment = input()
+    digit = int(apartment[0])
+    length = len(apartment)
+    answer = (digit - 1) * 10 + (length * (length + 1)) // 2
+    print(answer)
